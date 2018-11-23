02847a77c0615b0d08b46ac01219f7873182d1d5
AndrewL188 <ajluo2@illinois.edu>
2018-11-23 14:00:42 -0600

    Stones are now checked for whether or not they are captured at the start of each turn

--- networkGo/src/GoGameEngine.cpp
+++ networkGo/src/GoGameEngine.cpp
@@ -42,6 +42,7 @@ void GoGameEngine::playMove(int row, int col)
 
 	board_state_[row][col] = current_player_;
 	flat_board_state_[flatten(row,col)] = current_player_;
+	checkCaptures(row, col);
 
 	//Sets current_player to opposite color and pass_counter_ back to 0 (since passes must be consecutive)
 	current_player_ = (current_player_ == kBlackPlayer) ? kWhitePlayer : kBlackPlayer;
@@ -68,7 +69,13 @@ bool GoGameEngine::LegalMove(int row, int col)
 	if (board_state_[row][col] != kEmpty) {
 		return false;
 	}
-
+	//Checks if play is a suicide move, which is, in this implementation of go, illegal.
+	board_state_[row][col] == current_player_;
+	if (!hasOpenLiberties(findLiberties(row, col))) {
+		board_state_[row][col] == kEmpty;
+		return false;
+	}
+	board_state_[row][col] == kEmpty;
 	return true;
 }
 
